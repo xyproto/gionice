20699fc762c1ba71b1618a6d53c4e48a7aa4a4a4
Alexander F. Rødseth <rodseth@gmail.com>
2021-02-03 15:56:16 +0100

    Make the Go code slightly more ideomatic

--- gion.go
+++ gion.go
@@ -1,4 +1,4 @@
-// Converted from ionice in util-linux (GPL2 licensed) to a Go package
+// Package gion contains code that has been ported from util-linux/ionice (GPL2 licensed)
 package gion
 
 import (
@@ -21,12 +21,20 @@ const (
 	IOPRIO_CLASS_SHIFT = 13
 )
 
-// SetPri is "ioprio_set" in ion
+// SetPri sets the IO priority for the given which (process, pgrp or user) and who (the ID),
+// using the given io priority number.
 func SetPri(which, who int, ioprio uint) (uint, error) {
 	r1, _, errNo := syscall.Syscall(syscall.SYS_IOPRIO_SET, uintptr(which), uintptr(who), uintptr(ioprio))
-	//fmt.Println("FROM SYSCALL (a):", r1)
-	//fmt.Println("FROM SYSCALL (b):", r2)
-	//fmt.Println("FROM SYSCALL (err):", errNo)
+	var err error
+	if errNo != 0 {
+		err = errNo
+	}
+	return uint(r1), err
+}
+
+// Pri returns the IO priority for the given which (process, pgrp or user) and who (the ID).
+func Pri(which, who int) (uint, error) {
+	r1, _, errNo := syscall.Syscall(syscall.SYS_IOPRIO_GET, uintptr(which), uintptr(who), uintptr(0))
 	var err error
 	if errNo != 0 {
 		err = errNo
@@ -35,26 +43,14 @@ func SetPri(which, who int, ioprio uint) (uint, error) {
 	return uint(r1), err
 }
 
-// Pri is "ioprio_get" in ion
-func Pri(which, who int) (uint, error) {
-	r1, _, errNo := syscall.Syscall(syscall.SYS_IOPRIO_GET, uintptr(which), uintptr(who), uintptr(0))
-	//fmt.Println("FROM SYSCALL (a):", r1)
-	//fmt.Println("FROM SYSCALL (b):", r2)
-	//fmt.Println("FROM SYSCALL (err):", errNo)
-	var err error
-	if errNo != 0 {
-		err = errNo
-	}
-	// TODO: r1 or r2?
-	return uint(r1), err
-}
+type IOPRIO_CLASS int
 
 func IOPRIO_PRIO_MASK() uint {
 	return (uint(1) << IOPRIO_CLASS_SHIFT) - 1
 }
 
-func IOPRIO_PRIO_CLASS(mask uint) uint {
-	return mask >> IOPRIO_CLASS_SHIFT
+func IOPRIO_PRIO_CLASS(mask uint) IOPRIO_CLASS {
+	return IOPRIO_CLASS(mask >> IOPRIO_CLASS_SHIFT)
 }
 
 func IOPRIO_PRIO_DATA(mask uint) uint {
@@ -65,20 +61,22 @@ func IOPRIO_PRIO_VALUE(classn, data uint) uint {
 	return ((classn << IOPRIO_CLASS_SHIFT) | data)
 }
 
-var to_prio map[int]string = map[int]string{
+var to_prio = map[IOPRIO_CLASS]string{
 	IOPRIO_CLASS_NONE: "none",
 	IOPRIO_CLASS_RT:   "realtime",
 	IOPRIO_CLASS_BE:   "best-effort",
 	IOPRIO_CLASS_IDLE: "idle",
 }
 
-func Parse(str string) int {
-	for i := 0; i < len(to_prio); i++ {
-		if strings.ToLower(str) == strings.ToLower(to_prio[i]) {
-			return i
+// Parse converts a case-insensitive string containing either:
+// "none", "realtime", best-effort" or "idle", to a corresponding IOPRIO_CLASS.
+func Parse(str string) (IOPRIO_CLASS, error) {
+	for k, v := range to_prio {
+		if strings.ToLower(str) == strings.ToLower(v) {
+			return k, nil
 		}
 	}
-	return -1
+	return 0, fmt.Errorf("could not parse %s to an IOPRIO_CLASS constant", str)
 }
 
 func Print(pid, who int) {
@@ -88,8 +86,9 @@ func Print(pid, who int) {
 	}
 	ioclass := IOPRIO_PRIO_CLASS(ioprio)
 	name := "unknown"
-	if ioclass >= 0 && ioclass < uint(len(to_prio)) {
-		name = to_prio[int(ioclass)]
+	last_index := IOPRIO_CLASS(len(to_prio) - 1)
+	if ioclass >= 0 && ioclass <= last_index {
+		name = to_prio[ioclass]
 	}
 	if ioclass != IOPRIO_CLASS_IDLE {
 		fmt.Printf("%s: prio %d\n", name, IOPRIO_PRIO_DATA(ioprio))
@@ -98,7 +97,6 @@ func Print(pid, who int) {
 	}
 }
 
-// SetIDPri is "ioprio_setid" in ion
 func SetIDPri(which, ioclass, data, who int, tolerant bool) {
 	_, err := SetPri(who, which, IOPRIO_PRIO_VALUE(uint(ioclass), uint(data)))
 	if err != nil && !tolerant {
