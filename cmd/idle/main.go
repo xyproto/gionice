6847c421ab004af44b2b5c1fe87c92f1c585882e
Alexander F. Rødseth <rodseth@gmail.com>
2021-02-09 11:12:43 +0100

    Update example

--- cmd/idle/main.go
+++ cmd/idle/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 
@@ -12,11 +11,9 @@ func main() {
 	// Make the current process "idle" (level 7)
 	gionice.SetIdle(0)
 
-	// Write to a file and delete then delete it, repeatedly
+	// Generate I/O activity
 	for {
-		fmt.Println("TICK")
 		_ = ioutil.WriteFile("frenetic.dat", []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 0644)
-		fmt.Println("TOCK")
 		_ = os.Remove("frenetic.dat")
 	}
 }
