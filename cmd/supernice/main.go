9708594758812c2b4508d8c83ac20693473dd2ab
Alexander F. Rødseth <rodseth@gmail.com>
2021-02-16 15:35:47 +0100

    Add more examples

--- cmd/supernice/main.go
+++ cmd/supernice/main.go
@@ -7,12 +7,13 @@ import (
 	"github.com/xyproto/gionice"
 )
 
-func init() {
-	// Make the current process CPU priority to be "idle" (level 7)
-	gionice.SetIdle(0)
-}
-
 func main() {
+	// Set the current process group CPU niceness to 10
+	_ = gionice.Nice()
+
+	// Set the current process group IO priority to "idle" (level 7)
+	_ = gionice.Idle()
+
 	// Generate I/O activity
 	for {
 		_ = ioutil.WriteFile("frenetic.dat", []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 0644)
